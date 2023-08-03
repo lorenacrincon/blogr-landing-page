a43d0c5c2b79d3e1aaddfc2314ef55c1f82c05dd
Lorena Rincon <lorec.rincon@gmail.com>
2023-08-03 18:31:09 -0300

    Project Setup and Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./header.css";
+import Attribution from "./assets/components/Attribution";
+import Header from "./assets/components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Attribution />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Attribution.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Attribution() {
+  return (
+    <div className=" text-center text-slate-900">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-red-400"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/lorenacrincon"
+        target="_blank"
+        className="text-red-400"
+      >
+        Lorena Rincón
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Attribution;
